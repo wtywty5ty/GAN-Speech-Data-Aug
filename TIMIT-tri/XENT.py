a629ccd0791719480d4ed8f77f352930b328630f
wtywty5ty <wtywty5ty@icloud.com>
2018-08-17 11:34:32 +0100

    final

--- TIMIT-tri/XENT.py
+++ TIMIT-tri/XENT.py
@@ -11,19 +11,22 @@ with open('test_xent.pkl', 'rb') as f:
 with open('cv_xent.pkl', 'rb') as f:
     cv_xent = pickle.load(f)
 
-with open('fake_xent.pkl', 'rb') as f:
-    fake_xent = pickle.load(f)
-        
+with open('fake_con_xent.pkl', 'rb') as f:
+    fake_con_xent = pickle.load(f)
 
-labels = ['Training set', 'CV set', 'Test set', 'Generated data']
+with open('fake_uncon_xent.pkl', 'rb') as f:
+    fake_uncon_xent = pickle.load(f)      
+
+labels = ['Training set','CV set','Test set', 'CGAN', 'Uncon GAN']
     
 data = []
 data.append(train_xent)
 data.append(cv_xent)
 data.append(test_xent)
-data.append(fake_xent)
+data.append(fake_con_xent)
+data.append(fake_uncon_xent)
 
 plt.boxplot(data, labels=labels, showmeans=True, showfliers=False)
 plt.ylabel('Cross entropy loss')
-plt.savefig('average_xent.png')
+plt.savefig('average_uncon_xent.png')
 plt.close
--- TIMIT-tri/temp.py
+++ TIMIT-tri/temp.py
@@ -1,105 +1,72 @@
-"""
-from ProcessRawData import statemap
-import subprocess
-
-smap = statemap('states.map')
-for key in smap.keys():
-    subprocess.run("sed -i 's/%s/%s/' HTKFILE/mlabs/gan_equal_phone.mlf"%(key, smap[key]), shell=True)
-
-"""
-
-"""
-from genHTKfile import genHTKfile
-
-phone_list1 = ['b', 'd', 'f', 'g', 'k', 'l', 'm', 'n', 'p', 'r', 's', 't']
-phone_list2 = ['v', 'w', 'y', 'z', 'aa', 'ae', 'ah', 'ao', 'aw', 'ax', 'ay', 'ch']
-phone_list3 = ['cl', 'dh', 'dx', 'eh', 'el', 'en', 'er', 'ey', 'hh', 'ih', 'ix', 'iy']
-phone_list4 = ['jh', 'ng', 'ow', 'oy', 'sh', 'th', 'uh', 'uw', 'zh', 'epi', 'sil', 'vcl']
-phone_list = phone_list1 + phone_list2 + phone_list3 + phone_list4
-for phone in phone_list:
-    for ID in range(10):
-        task = genHTKfile(phone, ID)
-        task.appendmlf()
-
-
-
-import subprocess
-from utils.ProcessRawData import computeprior
-DIR = '/home/ty/tw472/triphone/temp.tri_Z/dnntrain'
-HTKcmd = '%s/hmm0/HNTrainSGD -B -C %s/basic.cfg -C %s/finetune.cfg -S %s/lib/flists/train.scp -l LABEL -I %s/train.mlf -H %s/hmm0/MMF -M %s/hmm0 %s/hmms.mlist' % (
-        DIR, DIR, DIR, DIR, DIR, DIR, DIR, DIR)
-
-s = subprocess.Popen(HTKcmd, shell=True, stdout=subprocess.PIPE)
-plist = computeprior(s)
-total = 0
-with open('prior.txt', 'w') as f:
-    for key in plist.keys():
-        total += plist[key]
-        f.write('%d %.6f\n' %(key, plist[key]))
-
-print(total)
-
-
-# HNTrainSGD
-DIR = '/home/ty/tw472/triphone/temp.tri_Z/dnntrain'
-HTKcmd = '%s/hmm0/HNTrainSGD -B -C %s/basic.cfg -C %s/finetune_gpu%d.cfg -S %s/lib/flists/train.scp -l LABEL -I %s/train.mlf -H %s/hmm0/MMF -M %s/hmm0 %s/hmms.mlist' % (
-            DIR, DIR, DIR, opt.gpu_id, DIR, DIR, DIR, DIR, DIR)
-
-s = subprocess.Popen(HTKcmd, shell=True, stdout=subprocess.PIPE)
-
-#for ... loop
-    l = s.stdout.read(4)
-    l = struct.unpack("i", l)
-    l = l[0]
-    label = s.stdout.read(l * 4)
-    label = struct.unpack("%di" % l, label)
-    size = s.stdout.read(8)
-    size = struct.unpack("ii", size)
-    data_size = size[0] * size[1]
-    data = s.stdout.read(data_size * 4)
-    data = struct.unpack("%df" % data_size, data)
-    # ...
-
-s.kill()
-
-# HNForward
-DIR = '/home/ty/tw472/triphone/FH7/dnntrain'
-HTKcmd = '%s/HNForward -C %s/basic.cfg -C %s/eval.cfg -H %s/hmm0/MMF %s/hmms.mlist'%(DIR,DIR,DIR,DIR,DIR)
-s = subprocess.Popen(HTKcmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
-#for loop ...
-    # pack data ...
-    s.stdin.write(binary_data)
-    rows = s.stdout.read(4)
-    rows = struct.unpack('i', rows)[0]
-    columns = s.stdout.read(4)
-    columns = struct.unpack('i',columns)[0]
-    results_list = []
-    for i in range(rows):
-        results = s.stdout.read(columns*4)
-        results = struct.unpack('%df'% columns, results)
-        results_list.append(results)
-end = struct.pack('i', 0)
-s.stdin.write(end)
-s.kill()
-"""
+import pickle 
+import numpy as np
+import subprocess 
+import struct
 from utils.ProcessRawData import *
-prior = {}
-phonemap = triphoneMap('slist.txt', 'aa')
-with open('prior.txt', 'r') as f:
-    for line in f.readlines():
-        tid = int(line.split(' ')[0])
-        state = phonemap.id2state[tid]
-        phone = state.split('_')[0]
-        pr = float(line.split(' ')[1].strip('\n'))
-        if phone in prior:
-            prior[phone] += pr
+# read test data from TIMIT
+
+
+
+
+DIR = '/home/ty/tw472/triphone/temp.tri_Z/dnntrain'
+HTKcmd = '%s/hmm0/HNTrainSGD -B -C %s/basic.cfg -C %s/finetune.cfg -S %s/lib/flists/dnn.cv.scp -l LABEL -I %s/train.mlf -H %s/hmm0/MMF -M %s/hmm0 %s/hmms.mlist' % (
+            DIR, DIR, DIR, DIR, DIR, DIR, DIR, DIR)
+
+'''
+DIR = '/home/ty/tw472/master/acoustic/GAN/dnntrain.3.uncon.prior'
+HTKcmd = '%s/hmm0/HNTrainSGD -B -C %s/basic.cfg -C %s/finetune.cfg -S %s/DATA/flists/uncon.prior.3.scp -l LABEL -I %s/DATA/mlabs/uncon.prior.3.mlf -H %s/hmm0/MMF -M %s/hmm0 %s/hmms.mlist' % (
+            DIR, DIR, DIR, DIR, DIR, DIR, DIR, DIR)
+'''
+'''
+DIR = '/home/ty/tw472/triphone/temp.tri_Z/dnntrain'
+HTKcmd = '%s/hmm0/HNTrainSGD -B -C %s/basic.cfg -C %s/finetune.cfg -S %s/lib/flists/test.scp -l LABEL -I %s/timit_test.mlf -H %s/hmm0/MMF -M %s/hmm0 %s/hmms.mlist'%(
+    DIR,DIR,DIR,DIR,DIR,DIR,DIR,DIR)
+'''
+data = subprocess.Popen(HTKcmd, shell=True, stdout=subprocess.PIPE)
+
+DIR = '/home/ty/tw472/triphone/FH7/dnntrain'
+HTKcmd = '%s/HNForward -C %s/basic.cfg -C %s/eval.cfg -H %s/hmm0/MMF %s/hmms.mlist' % (DIR, DIR, DIR, DIR, DIR)
+test = subprocess.Popen(HTKcmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
+
+xent_list = []
+last_batch =False
+while not last_batch:
+    llabel = data.stdout.read(4)
+    llabel = struct.unpack('i', llabel)[0]
+    label = data.stdout.read(llabel*4)
+    label = struct.unpack('%di'%llabel, label)
+    buf = data.stdout.read(llabel*640*4+8)
+    if llabel<10000:
+        last_batch = True
+
+    test.stdin.write(buf)
+    test.stdin.flush()
+    
+    rows = test.stdout.read(4)
+    rows = struct.unpack('i', rows)[0]
+    columns = test.stdout.read(4)
+    columns = struct.unpack('i',columns)[0]
+    for i in range(rows):
+        tid = label[i]
+        results = test.stdout.read(columns*4)
+        results = struct.unpack('%df'% columns, results)
+        results = np.array(results)
+        phonemap_temp = triphoneMap('slist.txt', 'aa')
+        phone = phonemap_temp.id2states[tid].split('_')[0]
+        phonemap = triphoneMap('slist.txt', phone)
+        tid_pool = [phonemap.f2t[i] for i in range(phonemap.nlabels())]
+        score_pool = [results[tid] for tid in tid_pool]
+        cls_score = max(score_pool)
+        cls_fid = score_pool.index(cls_score)
+        if tid == tid_pool[cls_fid]:
+            xent_list.append(1)
         else:
-            prior[phone] = pr
+            xent_list.append(0)
 
-with open('phone_prior.txt', 'w') as f:
-    for phone in prior.keys():
-        f.write('%s %f\n'%(phone, prior[phone]))
+#avgxent = {}
+#for tid in rdict.keys():
+#    avgxent[tid] = rdict[tid][0]/rdict[tid][1]
+print(np.mean(xent_list))
 
-
-
-
+data.kill()
+test.kill()
